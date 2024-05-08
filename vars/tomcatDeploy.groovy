edffdfba151553dd97f94ecc5fbf054adfcfa9ef
javahometech <hari.kammana@gmail.com>
2024-05-08 08:29:39 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,6 +1,6 @@
-def call(){
+def call(tomcatIp){
     sshagent(['tomcatdev-7am']) {
-    sh "scp -o StrictHostKeyChecking=no target/devops-app.war ec2-user@54.82.16.197:/opt/tomcat9/webapps"
-    sh "ssh ec2-user@54.82.16.197 /opt/tomcat9/bin/shutdown.sh"
-    sh "ssh ec2-user@54.82.16.197 /opt/tomcat9/bin/startup.sh"
+    sh "scp -o StrictHostKeyChecking=no target/devops-app.war ec2-user@${tomcatIp}:/opt/tomcat9/webapps"
+    sh "ssh ec2-user@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
+    sh "ssh ec2-user@${tomcatIp} /opt/tomcat9/bin/startup.sh"
 }
